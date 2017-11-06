76e0d98b4e4758d21a824ffb85914bb446c9fcf1
Thomas Allmer <d4kmor@gmail.com>
2017-11-06 17:50:59 +0100

    bugfix: setting namespace in example is no longer needed

--- demo/grain-translate-example.js
+++ demo/grain-translate-example.js
@@ -2,12 +2,6 @@ import GrainLitElement, { html } from '../../grain-lit-element/GrainLitElement.j
 import GrainTranslateMixin from '../GrainTranslateMixin.js';
 
 export default class GrainTranslateExample extends GrainTranslateMixin(GrainLitElement(HTMLElement)) {
-  static get translateDefaults() {
-    return {
-      namespace: 'grain-translate',
-    };
-  }
-
   render() {
     return html`
       <h3>${this.t('greeting')}</h3>
